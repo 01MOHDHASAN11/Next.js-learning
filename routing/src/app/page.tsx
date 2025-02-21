1c66a02d05bddb2a6c76917b0a4306ae8f3ce1a6
01MOHDHASAN11 <01mohdhasan11@gmail.com>
2025-02-21 22:20:14 +0530

    Started learning Next.js

---
+++ routing/src/app/page.tsx
@@ -0,0 +1,8 @@
+import HeroPic from "../components/hero"
+export default function Home() {
+  return (
+    <main className="w-full">
+      <HeroPic/>
+    </main>
+  );
+}
---
+++ routing/src/app/reliability/page.tsx
@@ -0,0 +1,7 @@
+export default function ReliabilityPage() {
+  return (
+    <main>
+        <h1>This is reliability page</h1>
+    </main>
+  )
+}
