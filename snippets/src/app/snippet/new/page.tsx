f58f5a8547fa66edbe08097b6308344eff551bfa
01MOHDHASAN11 <01mohdhasan11@gmail.com>
2025-02-25 16:44:14 +0530

    caching in next.js

--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -1,30 +1,18 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { prisma } from "@/lib/prisma"
-import { redirect } from "next/navigation"
 import Link from "next/link"
+import { useActionState } from "react"
+import * as actions from "@/actions"
 
-const page = () => {
+const Page = () => {
 
-    async function createSnippet(formData:FormData) {
-        "use server"
-        const title=formData.get("title") as string
-        const code=formData.get("code") as string
-
-        const snippet = await prisma.snippet.create({
-            data:{
-                title,
-                code
-            }
-        })
-        console.log("My snippet is: ",snippet)
-        redirect("/")
-    }
+  const [ShowFormErrorMessage,formFieldRequiredMessage] = useActionState(actions.createSnippet,{message:""})
 
   return (
-    <form action={createSnippet}>
+    <form action={formFieldRequiredMessage}>
       <div>
         <div>
           <h1 className="text-3xl font-bold">Create new snippet</h1>
@@ -32,13 +20,14 @@ const page = () => {
         <div className="mt-4">
         <Label>Title:</Label>
         </div>
-        <Input type="text" name="title" id="title" required/>
+        <Input type="text" name="title" id="title"/>
       </div>
       <div className="mt-4">
         <Label>Code:</Label>
-        <Textarea name="code" id="code" required/>
+        <Textarea name="code" id="code"/>
       </div>
       <div className="mt-4">
+      {ShowFormErrorMessage.message && <div className="bg-red-400 p-2 rounded font-medium text-xl text-center text-lime-50 mb-2 border-2 border-red-600">{ShowFormErrorMessage.message}**</div>}
       <Button type="submit">New</Button>
       <Link href={"/"} className="mx-3"><Button>Back</Button></Link>
       </div>
@@ -46,4 +35,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
